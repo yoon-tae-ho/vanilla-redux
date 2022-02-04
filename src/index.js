415acb93da6a0707cd8f26d99feaeb59fb25e18d
yoontaeho <pln0302@yonsei.ac.kr>
2022-02-04 16:48:47 +0900

    Learned about action

--- src/index.js
+++ src/index.js
@@ -4,10 +4,24 @@ const plus = document.querySelector("#plus");
 const minus = document.querySelector("#minus");
 const number = document.querySelector("span");
 
-const countModifier = (state = 0) => {
-  return state;
+const countModifier = (state = 0, action) => {
+  console.log(state, action);
+  const { type } = action;
+  if (type === "PLUS") {
+    return state + 1;
+  } else if (type === "MINUS") {
+    return state - 1;
+  } else {
+    return state;
+  }
 };
 
 const countStore = createStore(countModifier);
+
+countStore.dispatch({ type: "PLUS" });
+countStore.dispatch({ type: "PLUS" });
+countStore.dispatch({ type: "PLUS" });
+countStore.dispatch({ type: "PLUS" });
+countStore.dispatch({ type: "MINUS" });
 
 console.log(countStore.getState());
