19a7788fd39644fec326db8971021629a12565a7
yoontaeho <pln0302@yonsei.ac.kr>
2022-02-05 13:43:55 +0900

    Made toDoList using vanilla js

--- src/index.js
+++ src/index.js
@@ -1,27 +1,18 @@
-import { createStore } from "redux";
+const form = document.querySelector("form");
+const input = form.querySelector("input");
+const ul = document.querySelector("ul");
 
-const plus = document.querySelector("#plus");
-const minus = document.querySelector("#minus");
-const number = document.querySelector("span");
-
-number.innerText = 0;
-
-const PLUS = "PLUS";
-const MINUS = "MINUS";
-
-const countModifier = (state = 0, action) => {
-  switch (action.type) {
-    case PLUS:
-      return state + 1;
-    case MINUS:
-      return state - 1;
-    default:
-      return state;
-  }
+const createToDo = (text) => {
+  const li = document.createElement("li");
+  li.innerText = text;
+  ul.appendChild(li);
 };
 
-const countStore = createStore(countModifier);
-countStore.subscribe(() => (number.innerText = countStore.getState()));
+const handleSubmit = (event) => {
+  event.preventDefault();
+  const value = input.value;
+  input.value = "";
+  createToDo(value);
+};
 
-plus.addEventListener("click", () => countStore.dispatch({ type: PLUS }));
-minus.addEventListener("click", () => countStore.dispatch({ type: MINUS }));
+form.addEventListener("submit", handleSubmit);
